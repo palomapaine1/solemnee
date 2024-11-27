785a83cb807b5630da03bdb06cea91385d208efe
palomapaine1 <palomapaine4@gmail.com>
2024-11-27 18:42:27 -0300

    Update untitled6.py

--- untitled6.py
+++ untitled6.py
@@ -27,9 +27,6 @@ if response.status_code == 200:
     st.write(df.head())
 else:  # Corrected indentation here
     st.error('Error al obtener los datos de la API')
-    return None
-
-
 # Llamar la función para obtener los datos
 api_url = "https://restcountries.com/v3.1/all"
 df = obtener_datos_api(api_url)
